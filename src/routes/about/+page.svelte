7a98d46c821cd30ee6cf4adb5df5a828142fd823
Ryan Atkinson <mail@ryanatkn.com>
2024-11-20 22:31:47 -0700

    improve about page header

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,8 +4,6 @@
 	import Library_Footer from '@ryanatkn/fuz/Library_Footer.svelte';
 	import Community_Links_Panel from '@ryanatkn/fuz/Community_Links_Panel.svelte';
 	import Breadcrumb from '@ryanatkn/fuz/Breadcrumb.svelte';
-	import Svg from '@ryanatkn/fuz/Svg.svelte';
-	import {fuz_logo} from '@ryanatkn/fuz/logos.js';
 
 	import {package_json, src_json} from '$routes/package.js';
 
@@ -17,12 +15,10 @@
 <main class="width_md">
 	<section class="box">
 		<header class="box pt_xl">
-			<Svg data={fuz_logo} size="var(--icon_size_xl)" />
-			<h1 class="mt_xl2">fuz_template</h1>
+			<div style:--size="var(--size_xl)">
+				<Breadcrumb>{package_json.glyph}</Breadcrumb>
+			</div>
 		</header>
-		<div style:--size="var(--size_xl)">
-			<Breadcrumb>{package_json.glyph}</Breadcrumb>
-		</div>
 	</section>
 	<section class="box w_100 mb_lg">
 		<div class="panel p_lg width_md">
